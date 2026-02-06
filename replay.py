2c81b50b81faed9a635908f8f1d128d5b7c88126
Cole Wendrowski <blobspire@gmail.com>
2026-02-06 18:10:04 -0500

    Add replay with user intervention

---
+++ replay.py
@@ -0,0 +1,147 @@
+from fileinput import filename
+import pybullet as p
+import pybullet_data
+import numpy as np
+import os
+import time
+import json
+from robot import Panda
+from teleop import KeyboardController
+
+# Load and execute the saved states from the JSON file, while allowing user intervention.
+
+# parameters
+control_dt = 1. / 240.
+
+# create simulation and place camera
+physicsClient = p.connect(p.GUI)
+p.setGravity(0, 0, -9.81)
+# disable keyboard shortcuts so they do not interfere with keyboard control
+p.configureDebugVisualizer(p.COV_ENABLE_KEYBOARD_SHORTCUTS, 0)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.resetDebugVisualizerCamera(cameraDistance=1.0, 
+                                cameraYaw=40.0,
+                                cameraPitch=-30.0, 
+                                cameraTargetPosition=[0.5, 0.0, 0.2])
+
+# load the objects
+urdfRootPath = pybullet_data.getDataPath()
+plane = p.loadURDF(os.path.join(urdfRootPath, "plane.urdf"), basePosition=[0, 0, -0.625])
+table = p.loadURDF(os.path.join(urdfRootPath, "table/table.urdf"), basePosition=[0.5, 0, -0.625])
+cube1 = p.loadURDF(os.path.join(urdfRootPath, "cube_small.urdf"), basePosition=[0.6, -0.2, 0.05])
+cube2 = p.loadURDF(os.path.join(urdfRootPath, "cube_small.urdf"),
+                    basePosition=[0.7, 0.2, 0.05], baseOrientation=p.getQuaternionFromEuler([0, 0, 0.7]))
+
+# load the robot
+jointStartPositions = [0.0, 0.0, 0.0, -2*np.pi/4, 0.0, np.pi/2, np.pi/4, 0.0, 0.0, 0.04, 0.04]
+panda = Panda(basePosition=[0, 0, 0],
+                baseOrientation=p.getQuaternionFromEuler([0, 0, 0]),
+                jointStartPositions=jointStartPositions)
+
+# teleoperation interface
+teleop = KeyboardController()
+
+# run simulation
+# you can teleoperate the robot using the keyboard;
+# see "teleop.py" for the mapping between keys and motions
+state = panda.get_state()
+target_position = state["ee-position"]
+target_quaternion = state['ee-quaternion']
+
+# User defined constraints
+MIN_HEIGHT = 0.01
+ERROR = 0.001 # Threshold for considering the robot to have reached the target position
+
+gripper_open = True
+filename = 'state.json'
+
+user_intervention = False
+user_intervention_delay = 5 # Wait 5s after user intervention before allowing replay again
+user_intervention_delay_left = 0
+
+with open('state.json', 'r') as f:
+    states = json.load(f)
+    print("\nLoading State. You can intervene via keyboard teleop.")
+
+
+# Replay the saved states. If user intervenes, enter teleop loop until no input for a set time. Then resume replay. 
+# The intervention will occur after an action concludes so the user likely corrected the previous erronenous action and wants to continue replaying the saved states. 
+# Therefore, do not skip state frames.
+
+for state in states:
+
+    # Set the robot to the loaded joint positions
+    # joint_positions = state["joint-position"]
+    # panda.reset(joint_positions)
+    # Update the robot state
+    target_position = state["ee-position"]
+    target_quaternion = state["ee-quaternion"]
+    gripper_open = state["gripper-open"]
+    if gripper_open:
+        for _ in range(200):
+            panda.open_gripper()
+            p.stepSimulation()
+            time.sleep(control_dt)
+    else:
+        for _ in range(200):
+            panda.close_gripper()
+            p.stepSimulation()
+            time.sleep(control_dt)
+    
+    env_state = panda.get_state()
+    while np.linalg.norm(np.array(env_state["ee-position"]) - np.array(target_position)) > ERROR:
+        panda.move_to_pose(ee_position=target_position, ee_quaternion=target_quaternion, positionGain=0.025)
+        p.stepSimulation()
+        time.sleep(control_dt)
+        env_state = panda.get_state()
+
+    # Robot has executed a state. Now check for user intervention.
+    action = teleop.get_action()
+
+    # Detect user intervention during replay
+    if np.linalg.norm(action[0:6]) > 0 or action[6] != 0:
+        print(f"User intervention detected. Pausing replay for {user_intervention_delay} seconds and enabling teleop.\nContinued user intervention will renew the delay.")
+        user_intervention_delay_left = user_intervention_delay # Initiate user intervention delay timer
+
+    # We have the action that was used to intervene. Start with this so that it isn't lost. Get new action at end of loop.
+    while user_intervention_delay_left > 0:
+
+        target_position = target_position + action[0:3]
+
+        # Ensure that the target position is not within the table
+        if target_position[2] < MIN_HEIGHT:
+            target_position[2] = MIN_HEIGHT
+
+        target_quaternion = p.multiplyTransforms([0, 0, 0], p.getQuaternionFromEuler(action[3:6]),
+                                                    [0, 0, 0], target_quaternion)[1]
+        # move to the target pose
+        panda.move_to_pose(ee_position=target_position, ee_quaternion=target_quaternion)
+
+        # open or close the gripper
+        if action[6] == +1:
+            panda.open_gripper()
+            gripper_open = True
+        elif action[6] == -1:
+            panda.close_gripper()
+            gripper_open = False
+
+        # step simulation
+        p.stepSimulation()
+        time.sleep(control_dt)
+
+        action = teleop.get_action()
+        # If more intervention, reset the delay timer
+        if np.linalg.norm(action[0:6]) > 0 or action[6] != 0:
+            user_intervention_delay_left = user_intervention_delay # Initiate user intervention delay timer
+        else:
+            user_intervention_delay_left -= control_dt # Decrease delay timer
+
+        # Output time left for intervention each second
+        if user_intervention_delay_left < user_intervention_delay - 1 and int(user_intervention_delay_left) != int(user_intervention_delay_left + control_dt):
+            print(f"Resuming replay in {int(user_intervention_delay_left)} seconds...")
+
+
+
+    # step simulation
+    p.stepSimulation()
+    time.sleep(control_dt)
